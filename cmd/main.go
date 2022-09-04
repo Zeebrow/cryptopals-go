19bda2bc12d0f258e65351cbb6df68e155723898
Zeebrow <mzborowski@yahoo.com>
2022-09-04 17:51:39 -0400

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/Zeebrow/cryptopals-go/set1"
+import (
+	"github.com/Zeebrow/cryptopals-go/set1/set1_6"
+)
 
 func main() {
 	//set11Main()
@@ -8,5 +10,6 @@ func main() {
 	//set1.Set13Main()
 	// set1.Set14Main("textfiles/set1/set1_4_textfile.txt") // todo truncate output
 	//set15Main()
-	set1.Set16Main("textfiles/set1/set1_6_file.txt") // todo truncate output
+
+	set1_6.Set16Main("textfiles/set1/set16_file.txt") // todo truncate output
 }
